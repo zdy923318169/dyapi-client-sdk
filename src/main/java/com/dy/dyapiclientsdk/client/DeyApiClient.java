ecee7dab64899d92ab87f32e791ae3db63bf94cf
zhangsan <zhangsan@163.com>
2023-06-02 20:54:37 +0800

    Initial commit

---
+++ src/main/java/com/dy/dyapiclientsdk/client/DeyApiClient.java
@@ -0,0 +1,98 @@
+package com.dy.dyapiclientsdk.client;
+
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.dy.dyapiclientsdk.model.CityName;
+import com.dy.dyapiclientsdk.model.User;
+
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.dy.dyapiclientsdk.until.SignUntil.genSign;
+
+
+/**
+ * 调用第三方接口的客户端
+ */
+public class DeyApiClient {
+    private static final String GATEWAY_HOST = "http://localhost:8090";
+
+    private String accessKey;
+
+    private String secretKey;
+
+    public DeyApiClient(String accessKey, String secretKey) {
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+    }
+
+    public String getRandomNameByGet() {
+        //可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
+        HttpResponse httpResponse = HttpRequest.post(GATEWAY_HOST + "/api/name")
+                .addHeaders(getHeaderMap(""))
+                .body("")
+                .execute();
+        System.out.println(httpResponse.getStatus());
+        String result = httpResponse.body();
+        System.out.println(result);
+        return result;
+    }
+
+    public String getNameByPost(String name) {
+        //可以单独传入http参数，这样参数会自动做URL编码，拼接在URL中
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("name", name);
+        String result = HttpUtil.post(GATEWAY_HOST + "/api/name/", paramMap);
+        System.out.println(result);
+        return result;
+    }
+
+    private Map<String, String> getHeaderMap(String body) {
+        Map<String, String> hashMap = new HashMap<>();
+        hashMap.put("accessKey", accessKey);
+        // 一定不能直接发送
+//        hashMap.put("secretKey", secretKey);
+        hashMap.put("nonce", RandomUtil.randomNumbers(4));
+
+        try {
+            hashMap.put("body", URLEncoder.encode(body,"utf-8"));
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+
+        hashMap.put("timestamp", String.valueOf(System.currentTimeMillis() / 1000));
+        hashMap.put("sign", genSign(body, secretKey));
+        return hashMap;
+    }
+
+    public String getUsernameByPost(User user) {
+        String json = JSONUtil.toJsonStr(user);
+        HttpResponse httpResponse = HttpRequest.post(GATEWAY_HOST + "/api/name/user")
+                .addHeaders(getHeaderMap(json))
+                .body(json)
+                .execute();
+        System.out.println(httpResponse.getStatus());
+        String result = httpResponse.body();
+        System.out.println(result);
+        return result;
+    }
+
+    public String getWeatherInfoByPost(CityName cityName) {
+        String json = JSONUtil.toJsonStr(cityName);
+        HttpResponse httpResponse = HttpRequest.post(GATEWAY_HOST + "/api/weather/info")
+                .addHeaders(getHeaderMap(json))
+                .body(json)
+                .execute();
+        System.out.println(httpResponse.getStatus());
+        String result = httpResponse.body();
+        System.out.println(result);
+        return result;
+    }
+}
+
---
+++ src/main/java/com/dy/dyapiclientsdk/model/CityName.java
@@ -0,0 +1,13 @@
+package com.dy.dyapiclientsdk.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class CityName implements Serializable {
+    /**
+     *
+     */
+    private String cityName;
+}
